ef787231bf7bee1e9af78413e707466844f0c5d3
Mongkii <mongkii@hotmail.com>
2019-06-19 09:23:07 +0800

    Stylesheet sorted.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const gulp = require('gulp'),
     cleancss = require('gulp-clean-css'),
     htmlmin = require('gulp-htmlmin');
 
-gulp.task('move',()=>gulp.src('src/**')
+gulp.task('move', () => gulp.src('src/**')
     .pipe(gulp.dest('build')));
 
 gulp.task('m-js', () => gulp.src('src/**/*.js')
@@ -20,10 +20,11 @@ gulp.task('m-html', () => gulp.src('src/**/*.html')
     .pipe(htmlmin({
         removeComments: true,
         collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
         minifyJS: true,
         minifyCSS: true,
         minifyURLs: true
     }))
     .pipe(gulp.dest('build')));
 
-gulp.task('default',gulp.series('move',gulp.parallel('m-js','m-css','m-html')));
+gulp.task('default', gulp.series('move', gulp.parallel('m-js', 'm-css', 'm-html')));
